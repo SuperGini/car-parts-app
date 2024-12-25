72719ac05bc63c74c98053559f557d758a7bfc55
iordache <mihai.iordache82@gmail.com>
2024-12-25 20:13:02 +0200

    added html +css for search parts page

--- car-part-core-micro/angular/src/app/components/main/right/searchparts/search-parts.component.ts
+++ car-part-core-micro/angular/src/app/components/main/right/searchparts/search-parts.component.ts
@@ -1,10 +1,60 @@
 import {Component} from '@angular/core';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatSlider, MatSliderRangeThumb} from '@angular/material/slider';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {
+  MatCell, MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef,
+  MatRow, MatRowDef,
+  MatTable
+} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
-  selector: 'serach-parts-component',
+  selector: 'search-parts-component',
   templateUrl: 'search-parts.html',
-   styleUrl: 'search-parts.scss'
+  imports: [
+    MatFormField,
+    MatInput,
+    MatSlider,
+    MatSliderRangeThumb,
+    MatLabel,
+    FaIconComponent,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderRow,
+    MatRow,
+    MatPaginator,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRowDef,
+    MatRowDef
+  ],
+  styleUrl: 'search-parts.scss'
 })
 export class SearchPartsComponent {
 
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+
 }
+
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'}
+];
